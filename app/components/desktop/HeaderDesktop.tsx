8bb38c7bb806b58096964cd0dd97142fc55cf714
Ali Mousavi <96045566+Ali-Primer@users.noreply.github.com>
2024-11-28 15:34:59 +0330

    header done

--- app/components/desktop/HeaderDesktop.tsx
+++ app/components/desktop/HeaderDesktop.tsx
@@ -7,68 +7,69 @@ export function HeaderDesktop() {
       <div className="hidden w-full h-20 shadow-zinc-300 shadow-lg lg:flex justify-center">
         <div className="w-4/5 flex justify-between h-full">
           {/* right header */}
-          <div className="h-full items-center flex w-[48%] justify-between">
+          <div className="h-full items-center flex w-[40%] justify-between">
             <div className="flex items-center h-full">
               <Image
                 className="flex items-center"
                 alt="logo"
                 src="/logo/logobig.png"
-                width={120}
+                width={100}
                 height={100}
               />
             </div>
-            <div className="flex w-8/12 justify-between text-gray-600">
-              <div className="flex gap-x-2">
+            <div className="flex w-8/12 justify-between text-gray-600 text-sm">
+              <div className="flex gap-x-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
                   viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
+                  fill="currentColor"
+                  className="size-5"
                 >
                   <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z"
+                    fillRule="evenodd"
+                    d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                    clipRule="evenodd"
                   />
                 </svg>
-                فیلم معرفی
+                انتخاب شهر
               </div>
-              <div>
-                <Link href="/products">محصولات و خدمات</Link>
-              </div>
-              <div>تماس با ما</div>
             </div>
           </div>
 
           {/* left header */}
-          <div className="flex items-center justify-between w-2/5 text-gray-600">
-            <button className="flex gap-x-2 bg-gray-300 py-2 px-4">
+          <div className="flex items-center justify-between w-7/12 text-gray-600">
+            <button
+              className="flex gap-x-1 text-sm border-gray-400 border py-1 px-3"
+              style={{ boxShadow: "3px 3px 0px 0px rgba(213,213,233,1)" }}
+            >
+              نشان شده ها
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="size-6 mt-[2px]"
+                className="size-5"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
                 />
               </svg>
-              دانلود اپلیکیشن
             </button>
-            <div className="flex gap-x-2">
+            <button
+              className="flex gap-x-2 text-sm border-gray-400 border py-1 px-3"
+              style={{ boxShadow: "3px 3px 0px 0px rgba(213,213,233,1)" }}
+            >
+              سبد سفارش
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="size-6 mt-[2px]"
+                className="size-5"
               >
                 <path
                   strokeLinecap="round"
@@ -76,23 +77,48 @@ export function HeaderDesktop() {
                   d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
                 />
               </svg>
-              سبد سفارش
-            </div>
-            <div className="flex gap-x-2">
+            </button>
+            <button
+              className="flex gap-x-2 text-sm border-gray-400 border py-1 px-3"
+              style={{ boxShadow: "3px 3px 0px 0px rgba(213,213,233,1)" }}
+            >
+              پروفایل
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="black"
-                className="size-6 mt-[2px]"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-5"
               >
                 <path
-                  fillRule="evenodd"
-                  d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-5.5-2.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0ZM10 12a5.99 5.99 0 0 0-4.793 2.39A6.483 6.483 0 0 0 10 16.5a6.483 6.483 0 0 0 4.793-2.11A5.99 5.99 0 0 0 10 12Z"
-                  clipRule="evenodd"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
                 />
               </svg>
-              ورود | ثبت نام
-            </div>
+            </button>
+
+            <button
+              className="flex gap-x-2 text-sm border-gray-400 border py-1 px-3"
+              style={{ boxShadow: "3px 3px 0px 0px rgba(213,213,233,1)" }}
+            >
+              اعلان
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+                />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
